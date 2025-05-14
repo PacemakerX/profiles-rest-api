5bb464daa9ba0f4f0ddf8da974f74975b0d5986d
PacemakerX <sparsh.officialwork@gmail.com>
2025-05-14 14:27:59 +0530

    feat(api): Add PUT,PATCH AND DELETE method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from profiles_api import serializers
+
 class GenericAPIView(APIView):
     """A generic API view that can be extended for various purposes."""
     
@@ -11,7 +12,7 @@ class GenericAPIView(APIView):
     def get(self, request, format=None):
         """Handle GET requests."""
         
-        an_apiview= [
+        an_apiview = [
             "Uses HTTP methods as function (get, post, patch, put, delete)",
             "Is similar to a traditional Django view",
             "Gives you the most control over your logic",
@@ -20,7 +21,7 @@ class GenericAPIView(APIView):
         return Response({"message": "This is a generic GET response", "an_apiview": an_apiview})
 
     def post(self, request):
-        """Create POST request with our name ."""
+        """Create POST request with our name."""
         
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -29,3 +30,15 @@ class GenericAPIView(APIView):
             return Response({"message": message}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handle updating an object."""
+        return Response({"message": "PUT method called"})
+
+    def patch(self, request, pk=None):
+        """Handle partial update of an object."""
+        return Response({"message": "PATCH method called"})
+
+    def delete(self, request, pk=None):
+        """Handle deletion of an object."""
+        return Response({"message": "DELETE method called"}, status=status.HTTP_204_NO_CONTENT)
