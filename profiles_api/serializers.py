70dbd950b9ebf7ee0712a389093a8f3966fb1e3f
PacemakerX <sparsh.officialwork@gmail.com>
2025-05-24 02:03:08 +0530

    init(api): Add serializer and permissions

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,30 @@
 from rest_framework import serializers
-
+from profiles_api import models
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView."""
     name = serializers.CharField(max_length=10) # serializer takes care of validation
     
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Seraialize a user profile object"""
+    
+    class Meta:
+        model = models.UserProfile
+        fields=('id','email','name','password')
+        extra_kwargs={
+            'password':{
+                'write_only':True,
+                'style':{
+                    'input':'password'
+                }
+            }
+        }
+    
+    def create(self,validatede_data):
+        """Create and a return user data"""
+        user=models.UserProfile.objects.create(
+            email=validatede_data['email'],
+            name=validatede_data['name'],
+            password=validatede_data['password']
+        )
+        
+        return user
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from .views import GenericAPIView,HelloViewSet
+from .views import GenericAPIView,HelloViewSet,UserProfileViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello-viewset', HelloViewSet, basename='hello-viewset')
+router.register('profile',UserProfileViewSet,)
 urlpatterns = [
     path('', include(router.urls)),
     path('generic/', GenericAPIView.as_view(), name='generic-api'),
